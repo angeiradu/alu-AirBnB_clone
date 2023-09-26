677b660f2b29f79f64993402f3add932732241b9
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-09-26 00:46:33 -0700

    Update test_city.py

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module documentation
+"""
+
+import unittest
+from models.base_model import BaseModel
+from models.city import City
+
+
+class TestCity(unittest.TestCase):
+    """ Test the CIty Class """
+
+    def test_instance(self):
+        """ Test instance """
+        obj = City()
+        self.assertIsInstance(obj, City)
+
+    def test_is_subclass(self):
+        """test the instance of sub classes"""
+        city = City()
+        self.assertTrue(issubclass(type(city), BaseModel))
+
+    def test_name(self):
+        """test name"""
+        city = City()
+        self.assertEqual(city.name, "")
+        city.name = "Kigali"
+        self.assertEqual(city.name, "Kigali")
+
+    def test_city_id(self):
+        """test city id"""
+        city = City()
+        self.assertEqual(city.state_id, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
